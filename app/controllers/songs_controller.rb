8e12a24a7e352c5e91c3e7426ba6e3aec8510c5d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-08 02:08:32 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,10 @@ class SongsController < ApplicationController
   end
 
   def create
+<<<<<<< HEAD
+=======
+    binding.pry
+>>>>>>> 182475e2227f6f7dfa9d7b45551a6fccf62135d5
     @song = Song.create(song_params)
     if @song.save
       redirect_to song_path(@song)
@@ -25,6 +29,7 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+<<<<<<< HEAD
   def update
     @song = Song.find(params[:id])
     song_replacement = Song.create(song_params)
@@ -40,10 +45,16 @@ class SongsController < ApplicationController
     Song.find(params[:id]).destroy
     redirect_to songs_path
   end
+=======
+>>>>>>> 182475e2227f6f7dfa9d7b45551a6fccf62135d5
 
   private
 
   def song_params
+<<<<<<< HEAD
     params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre)
+=======
+    params.permit(:title, :artist_name, :release_year, :released, :genre)
+>>>>>>> 182475e2227f6f7dfa9d7b45551a6fccf62135d5
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :songs, only: [:index, :new, :edit, :show, :create, :update, :destroy]
+=======
+  resources :songs, only: [:index, :new, :edit, :show, :create, :update]
+>>>>>>> 182475e2227f6f7dfa9d7b45551a6fccf62135d5
 end
